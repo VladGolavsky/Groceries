59e64748c7b55ed513f3b2072b047cc2f8138dce
USER <opengeeklabvg@gmail.com>
2023-05-15 17:59:06 +0300

    add persistor

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { StatusBar, Platform } from 'react-native';
 import RootNavigation from 'src/navigation';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { lightGray } from 'src/constants/colors';
-import store from './redux';
+import store, { persistor } from './redux';
 
 export default () => {
   return (
     <>
       <Provider store={store}>
-        {Platform.OS === 'android' && <StatusBar barStyle={"dark-content"} backgroundColor={lightGray} />}
-        <RootNavigation />
+        <PersistGate loading={null} persistor={persistor}>
+          {Platform.OS === 'android' && <StatusBar barStyle={"dark-content"} backgroundColor={lightGray} />}
+          <RootNavigation />
+        </PersistGate>
       </Provider>
     </>
   );
